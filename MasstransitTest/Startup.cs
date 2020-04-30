7e6960c9279250902b5c517aaba195497e5005f0
丁松杰 <377973147@qq.com>
2020-04-30 11:24:08 +0800

    完成 分布式事务demo

--- MasstransitTest/Startup.cs
+++ MasstransitTest/Startup.cs
@@ -39,57 +39,73 @@ namespace MasstransitTest
 
                     cfg.UseHealthCheck(provider);
 
-                    cfg.ReceiveEndpoint("DeductStock", ep =>
-                    {
-                        //PrefetchCount 在 in memory bus 中无效 
-                        ep.ExecuteActivityHost<DeductStockActivity, DeductStockModel>(new Uri("loopback://localhost/DeductStock-Compensate"));
-                    });
-
-                    cfg.ReceiveEndpoint("DeductStock-Compensate", ep =>
-                    {
-                        ep.CompensateActivityHost<DeductStockActivity, DeductStockLog>(conf=> {
-                            conf.UseRetry(policy =>
-                            {
-                                policy.Exponential(5, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(10), TimeSpan.FromSeconds(2));
-                            });
-                        });
-                        ep.UseScheduledRedelivery(r => r.Intervals(TimeSpan.FromMinutes(5), TimeSpan.FromMinutes(10)));
-                    });
-                    cfg.ReceiveEndpoint("DeductBalance", ep =>
-                    {
-                        ep.ExecuteActivityHost<DeductBalanceActivity, DeductBalanceModel>(new Uri("loopback://localhost/DeductBalance-Compensate"));
-                    });
-
-                    cfg.ReceiveEndpoint("DeductBalance-Compensate", ep =>
-                    {
-                        ep.CompensateActivityHost<DeductBalanceActivity, DeductBalanceLog>(conf => {
-                            conf.UseRetry(policy =>
-                            {
-                                policy.Exponential(5, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(10), TimeSpan.FromSeconds(2));
-                            });
-                        });
-                        ep.UseScheduledRedelivery(r => r.Intervals(TimeSpan.FromMinutes(5), TimeSpan.FromMinutes(10)));
-                    });
-
-                    cfg.ReceiveEndpoint("CreateOrder", ep =>
-                    {
-                        ep.ExecuteActivityHost<CreateOrderActivity, CreateOrderModel>(new Uri("loopback://localhost/CreateOrder-Compensate"));
-                    });
-
-                    cfg.ReceiveEndpoint("Activity-Request", ep =>
-                    {
-                        var requestProxy = new CreateOrderRequestProxy();
-                        var responseProxy = new CreateOrderResponseProxy();
-                        ep.Instance(requestProxy);
-                        ep.Instance(responseProxy);
-                    });
+                    AddActivity(cfg);
                     cfg.ConfigureEndpoints(provider);
-                    
+
                 }));
                 x.AddRequestClient<CreateOrderCommand>();
 
             });
             services.AddMassTransitHostedService();
+        }
+
+        private static void AddActivity(IInMemoryBusFactoryConfigurator cfg)
+        {
+            #region CreateOrderRequest
+            #region DeductStock
+            cfg.ReceiveEndpoint("DeductStock", ep =>
+    {
+        //PrefetchCount 在 in memory bus 中无效 
+        ep.ExecuteActivityHost<DeductStockActivity, DeductStockModel>(new Uri("loopback://localhost/DeductStock-Compensate"));
+    });
+
+            cfg.ReceiveEndpoint("DeductStock-Compensate", ep =>
+            {
+                ep.CompensateActivityHost<DeductStockActivity, DeductStockLog>(conf =>
+                {
+                    conf.UseRetry(policy =>
+                    {
+                        policy.Exponential(5, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(10), TimeSpan.FromSeconds(2));
+                    });
+                });
+                ep.UseScheduledRedelivery(r => r.Intervals(TimeSpan.FromMinutes(5), TimeSpan.FromMinutes(10)));
+            });
+            #endregion
+
+            #region DeductBalance
+            cfg.ReceiveEndpoint("DeductBalance", ep =>
+                {
+                    ep.ExecuteActivityHost<DeductBalanceActivity, DeductBalanceModel>(new Uri("loopback://localhost/DeductBalance-Compensate"));
+                });
+
+            cfg.ReceiveEndpoint("DeductBalance-Compensate", ep =>
+            {
+                ep.CompensateActivityHost<DeductBalanceActivity, DeductBalanceLog>(conf =>
+                {
+                    conf.UseRetry(policy =>
+                    {
+                        policy.Exponential(5, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(10), TimeSpan.FromSeconds(2));
+                    });
+                });
+                ep.UseScheduledRedelivery(r => r.Intervals(TimeSpan.FromMinutes(5), TimeSpan.FromMinutes(10)));
+            });
+            #endregion
+
+            #region CreateOrder
+            cfg.ReceiveEndpoint("CreateOrder", ep =>
+                {
+                    ep.ExecuteActivityHost<CreateOrderActivity, CreateOrderModel>(new Uri("loopback://localhost/CreateOrder-Compensate"));
+                });
+
+            cfg.ReceiveEndpoint("Activity-Request", ep =>
+            {
+                var requestProxy = new CreateOrderRequestProxy();
+                var responseProxy = new CreateOrderResponseProxy();
+                ep.Instance(requestProxy);
+                ep.Instance(responseProxy);
+            });
+            #endregion 
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
